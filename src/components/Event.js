36b527477b69a66d9bfc04c568402a20c586e8f6
Atsukin <atsuki.tamura.2740@gmail.com>
2021-06-12 19:01:55 +0900

    refactoring

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,20 +1,22 @@
 import React from "react";
 
-const Event = ({ event }) => {
-            const id = event.id
-            const handleClickDeleteButton = () => {
-                dispatch({ type: 'DELETE_EVENT', id })
-            }
-            return (
-                <tr key={index}>
-                    <td>{event.id}</td>
-                    <td>{event.title}</td>
-                    <td>{event.body}</td>
-                    <td>
-                        <button type="button" className="btn btn-danger" onClick={handleClickDeleteButton}>削除</button>
-                    </td>
-                </tr>
-            )
+const Event = ({ dispatch, event }) => {
+    const id = event.id
+    const handleClickDeleteButton = () => {
+        const result = window.confirm(`イベント(id=${id})を本当に削除しても良いですか？`)
+        if (result) dispatch({ type: 'DELETE_EVENT', id })
+    }
+
+    return (
+        <tr>
+            <td>{event.id}</td>
+            <td>{event.title}</td>
+            <td>{event.body}</td>
+            <td>
+                <button type="button" className="btn btn-danger" onClick={handleClickDeleteButton}>削除</button>
+            </td>
+        </tr>
+    )
 }
 
 
