805c92b60b560f3fed2509707b54f5aeed852989
Atsukin <atsuki.tamura.2740@gmail.com>
2021-06-13 08:54:52 +0900

    react-hooks-provider

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,9 @@
 import React, { useReducer } from 'react'
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import EventForm from './EventForm';
 import Events from './Events';
-
+import AppContext from '../contexts/AppContext';
 import reducer from '../reducers';
 
 const App = () => {
@@ -12,10 +11,10 @@ const App = () => {
     console.log(state, ' in App.js ')
 
   return (
-    <div className="container-fluid">
+    <AppContext.Provider value={'Hello, I am a Provider.'}>
       <EventForm state={state} dispatch={dispatch} />
       <Events state={state} dispatch={dispatch} />
-    </div>
+    </AppContext.Provider>
   );
 }
 
